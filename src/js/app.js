b8e1498038122890f9aa2c50021442152023237d
iamspoo <spoorthi246@gmail.com>
2021-03-07 17:28:44 +0530

    index.html

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import "../css/style.css"
+
+import { default as Web3} from "web3"
+import { default as contract } from "truffle-contract"
+
+//import votingArtifacts from "../../build/contracts/Voting.json"
+//var VotingContract = contract(votingArtifacts)
