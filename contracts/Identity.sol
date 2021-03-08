a4836d0a41ce14fa23816d428a89404fb5ab0da9
Sindhura17 <sindhura.l.bhat@gmail.com>
2021-03-08 22:47:39 +0530

    Update Identity.sol

--- contracts/Identity.sol
+++ contracts/Identity.sol
@@ -17,7 +17,13 @@ contract Identity {
         bool doesExist; 
     }
     
+    struct Organisation {
+        bytes32 password; 
+        bool doesExist; 
+    }
+    
     mapping (bytes32 => User) users;
+    mapping (bytes32 => Organisation) org;
 
     function addUser(bytes32 username, bytes32 password) onlyOwner public {
         if (users[username].doesExist == false){
@@ -29,9 +35,26 @@ contract Identity {
         }
         
     }
-   
-    function validate(bytes32 username, bytes32 password) view public returns (bool) {
-        if(users[username].password==password){
+    function addOrg(bytes32 username, bytes32 password) onlyOwner public {
+        if (org[username].doesExist == false){
+            org[username] = Organisation(password,true);
+            //emit AddedCandidate(candidateID);
+        }
+        else{
+            revert("Username already exists");
+        }
+        
+    }
+
+    function validateUser(bytes32 username, bytes32 pswd) view public returns (bool) {
+        if(users[username].password==pswd){
+            return true;
+        }
+        return false;
+    }
+
+    function validateOrg(bytes32 username, bytes32 pswd) view public returns (bool) {
+        if(org[username].password==pswd){
             return true;
         }
         return false;
